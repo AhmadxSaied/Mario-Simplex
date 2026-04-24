850c0a1856ba99736e27b7034cbab5f986f3fec3
AhmadxSaied <ahmadsaied404@gmail.com>
2026-04-24 21:09:43 +0300

    initial commit

---
+++ mario-simplex/src/main/java/com/simplex/mario_simplex/MarioSimplexApplication.java
@@ -0,0 +1,13 @@
+package com.simplex.mario_simplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarioSimplexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarioSimplexApplication.class, args);
+	}
+
+}
---
+++ mario-simplex/src/test/java/com/simplex/mario_simplex/MarioSimplexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplex.mario_simplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarioSimplexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
